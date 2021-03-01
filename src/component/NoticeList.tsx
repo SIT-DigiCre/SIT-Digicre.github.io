8fb6c4d667ed9c24460882325fb37e616ab3b400
Shimiiy <bangchaq2002@gmail.com>
2021-03-02 00:33:21 +0900

    made お知らせ block able to scroll with overflow

--- src/component/NoticeList.tsx
+++ src/component/NoticeList.tsx
@@ -1,22 +1,18 @@
-import React, { useState } from 'react';
-import { ListGroup, Modal } from 'react-bootstrap';
-import { NoticesItem } from '../common';
+import React, { useState } from "react";
+import { ListGroup, Modal } from "react-bootstrap";
+import { NoticesItem } from "../common";
 
 const NoticeList: React.FC<Props> = (props) => {
+  const style = {};
+
   return (
-    <div>
-      <ListGroup>
-        {
-          props.noticesItemList.map((noticesItem: NoticesItem) => (
-            <ListGroup.Item>
-              {noticesItem.date + " - " + noticesItem.text}
-            </ListGroup.Item>
-          ))
-        }
-      </ListGroup>
-    </div>
-  )
-}
+    <ListGroup className="overflow-auto" style={{ height: 450 }}>
+      {props.noticesItemList.map((noticesItem: NoticesItem) => (
+        <ListGroup.Item>{noticesItem.date + " - " + noticesItem.text}</ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+};
 type Props = {
   noticesItemList: NoticesItem[];
 };
