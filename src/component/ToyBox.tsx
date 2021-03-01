bcdd9e7293aed0bb7853eea4522f0ce73d8bf7dc
Shimiiy <bangchaq2002@gmail.com>
2021-03-01 11:21:10 +0900

    added ToyBox.tsx

---
+++ src/component/ToyBox.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import ContentBlock from './ContentBlock';
+
+const ToyBox: React.FC<Props> = (props) => {
+  return (
+    <div>
+      {/* おもちゃ箱の実装 */}
+    </div>
+  );
+}
+
+type Props = {};
+
+export default ToyBox;
