bcf2300dd35b8a311ddbaac30efd79561cd3f21c
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-03-02 01:26:50 +0900

    change omoshirobako image

--- src/component/ToyBox.tsx
+++ src/component/ToyBox.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Card } from "react-bootstrap";
 
 const ToyBox: React.FC<Props> = (props) => {
   return (
-    <div>
-      {/* おもちゃ箱の実装 */}
-    </div>
+    <Card className="h-100">
+      <Card.Body>
+        <Card.Title style={{ fontSize: 25 }}><img src="./image/omochabako.png" className="img-fluid"/></Card.Title>
+        <Card.Text>おもしろコンテンツを準備中です</Card.Text>
+      </Card.Body>
+    </Card>
   );
-}
+};
 
 type Props = {};
 
