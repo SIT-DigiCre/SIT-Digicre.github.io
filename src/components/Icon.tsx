df12848b7d11fb8cbbb4615cb42e8e1eed8a19c4
newt <ysti2019.win2020.st@gmail.com>
2025-11-08 01:55:17 +0900

    fix: biome rule

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -21,7 +21,6 @@ export function MaterialSymbolsOpenInNew(props: SVGProps<SVGSVGElement>) {
 
 export function MaterialSymbolsInfo(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -29,6 +28,7 @@ export function MaterialSymbolsInfo(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>情報</title>
       <path
         fill="currentColor"
         d="M11 17h2v-6h-2zm1-8q.425 0 .713-.288T13 8q0-.425-.288-.712T12 7q-.425 0-.712.288T11 8q0 .425.288.713T12 9m0 13q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22"
@@ -39,7 +39,6 @@ export function MaterialSymbolsInfo(props: SVGProps<SVGSVGElement>) {
 
 export function MaterialSymbolsMail(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -47,6 +46,7 @@ export function MaterialSymbolsMail(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>メール</title>
       <path
         fill="currentColor"
         d="M4 20q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.587 1.413T20 20zm8-7l8-5V6l-8 5l-8-5v2z"
@@ -57,7 +57,6 @@ export function MaterialSymbolsMail(props: SVGProps<SVGSVGElement>) {
 
 export function MaterialSymbolsArticle(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -65,6 +64,7 @@ export function MaterialSymbolsArticle(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>記事</title>
       <path
         fill="currentColor"
         d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21zm2-4h7v-2H7zm0-4h10v-2H7zm0-4h10V7H7z"
@@ -75,7 +75,6 @@ export function MaterialSymbolsArticle(props: SVGProps<SVGSVGElement>) {
 
 export function MaterialSymbolsSchool(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -83,6 +82,7 @@ export function MaterialSymbolsSchool(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>学校</title>
       <path
         fill="currentColor"
         d="M21 17v-6.9L12 15L1 9l11-6l11 6v8zm-9 4l-7-3.8v-5l7 3.8l7-3.8v5z"
@@ -93,7 +93,6 @@ export function MaterialSymbolsSchool(props: SVGProps<SVGSVGElement>) {
 
 export function MaterialSymbolsLogin(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -101,6 +100,7 @@ export function MaterialSymbolsLogin(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>応募</title>
       <path
         fill="currentColor"
         d="M12 21v-2h7V5h-7V3h7q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21zm-2-4l-1.375-1.45l2.55-2.55H3v-2h8.175l-2.55-2.55L10 7l5 5z"
@@ -111,7 +111,6 @@ export function MaterialSymbolsLogin(props: SVGProps<SVGSVGElement>) {
 
 export function SimpleIconsX(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -119,6 +118,7 @@ export function SimpleIconsX(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>X</title>
       <path
         fill="currentColor"
         d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584l-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
@@ -129,7 +129,6 @@ export function SimpleIconsX(props: SVGProps<SVGSVGElement>) {
 
 export function SimpleIconsYoutube(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -137,6 +136,7 @@ export function SimpleIconsYoutube(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>YouTube</title>
       <path
         fill="currentColor"
         d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814M9.545 15.568V8.432L15.818 12z"
@@ -147,7 +147,6 @@ export function SimpleIconsYoutube(props: SVGProps<SVGSVGElement>) {
 
 export function SimpleIconsGithub(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -155,6 +154,7 @@ export function SimpleIconsGithub(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>GitHub</title>
       <path
         fill="currentColor"
         d="M12 .297c-6.63 0-12 5.373-12 12c0 5.303 3.438 9.8 8.205 11.385c.6.113.82-.258.82-.577c0-.285-.01-1.04-.015-2.04c-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729c1.205.084 1.838 1.236 1.838 1.236c1.07 1.835 2.809 1.305 3.495.998c.108-.776.417-1.305.76-1.605c-2.665-.3-5.466-1.332-5.466-5.93c0-1.31.465-2.38 1.235-3.22c-.135-.303-.54-1.523.105-3.176c0 0 1.005-.322 3.3 1.23c.96-.267 1.98-.399 3-.405c1.02.006 2.04.138 3 .405c2.28-1.552 3.285-1.23 3.285-1.23c.645 1.653.24 2.873.12 3.176c.765.84 1.23 1.91 1.23 3.22c0 4.61-2.805 5.625-5.475 5.92c.42.36.81 1.096.81 2.22c0 1.606-.015 2.896-.015 3.286c0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
@@ -222,7 +222,6 @@ export function MaterialSymbolsClose(props: SVGProps<SVGSVGElement>) {
 
 export function MaterialSymbolsGroups(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -230,6 +229,7 @@ export function MaterialSymbolsGroups(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>班</title>
       <path
         fill="currentColor"
         d="M0 18v-1.575q0-1.1 1.113-1.763T4 14q.325 0 .625.013t.575.062q-.35.5-.525 1.075T4.5 16.375V18H0Zm6 0v-1.625q0-1.625 1.663-2.625t4.337-1q2.7 0 4.35 1T18 16.375V18H6Zm13.5 0v-1.625q0-.65-.163-1.225t-.487-1.075q.275-.05.563-.062T20 14q1.8 0 2.9.663t1.1 1.762V18h-4.5ZM4 13q-.825 0-1.413-.588T2 11q0-.85.588-1.425T4 9q.85 0 1.425.575T6 11q0 .825-.575 1.413T4 13Zm16 0q-.825 0-1.413-.588T18 11q0-.85.588-1.425T20 9q.85 0 1.425.575T22 11q0 .825-.575 1.413T20 13Zm-8-1q-1.25 0-2.125-.875T9 9q0-1.275.875-2.138T12 6q1.275 0 2.138.863T15 9q0 1.25-.863 2.125T12 12Z"
@@ -240,7 +240,6 @@ export function MaterialSymbolsGroups(props: SVGProps<SVGSVGElement>) {
 
 export function MaterialSymbolsHelp(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -248,6 +247,7 @@ export function MaterialSymbolsHelp(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>ヘルプ</title>
       <path
         fill="currentColor"
         d="M10.6 16q0-2.025.363-2.913T12.5 11.15q1.025-.9 1.563-1.563t.537-1.512q0-1.025-.687-1.7T12 5.7q-1.275 0-1.938.775T9.126 8.05L6.55 6.95q.525-1.6 1.925-2.775T12 3q2.625 0 4.038 1.463t1.412 3.512q0 1.25-.537 2.138t-1.688 2.012Q14 13.3 13.738 13.913T13.475 16H10.6Zm1.4 6q-.825 0-1.412-.588T10 20q0-.825.588-1.413T12 18q.825 0 1.413.588T14 20q0 .825-.588 1.413T12 22Z"
@@ -256,9 +256,10 @@ export function MaterialSymbolsHelp(props: SVGProps<SVGSVGElement>) {
   );
 }
 
-export function MaterialSymbolsKeyboardArrowDown(props: SVGProps<SVGSVGElement>) {
+export function MaterialSymbolsKeyboardArrowDown(
+  props: SVGProps<SVGSVGElement>,
+) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -266,6 +267,7 @@ export function MaterialSymbolsKeyboardArrowDown(props: SVGProps<SVGSVGElement>)
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>下向きの矢印</title>
       <path
         fill="currentColor"
         d="m12 20l-8-8l1.4-1.425l5.6 5.6V4h2v12.175l5.6-5.6L20 12l-8 8Z"
@@ -276,7 +278,6 @@ export function MaterialSymbolsKeyboardArrowDown(props: SVGProps<SVGSVGElement>)
 
 export function SimpleIconsLine(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -284,6 +285,7 @@ export function SimpleIconsLine(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       {...props}
     >
+      <title>LINE</title>
       <path
         fill="currentColor"
         d="M19.365 9.863a.631.631 0 0 1 0 1.261H17.61v1.125h1.755a.63.63 0 1 1 0 1.259h-2.386a.631.631 0 0 1-.627-.629V8.108c0-.345.282-.63.63-.63h2.386a.63.63 0 0 1-.003 1.26H17.61v1.125h1.755zm-3.855 3.016a.63.63 0 0 1-.631.627a.618.618 0 0 1-.51-.25l-2.443-3.317v2.94a.63.63 0 0 1-1.257 0V8.108a.627.627 0 0 1 .624-.628c.195 0 .375.104.495.254l2.462 3.33V8.108c0-.345.282-.63.63-.63c.345 0 .63.285.63.63v4.771zm-5.741 0a.632.632 0 0 1-.631.629a.631.631 0 0 1-.627-.629V8.108c0-.345.282-.63.63-.63c.346 0 .628.285.628.63v4.771zm-2.466.629H4.917a.634.634 0 0 1-.63-.629V8.108c0-.345.285-.63.63-.63c.348 0 .63.285.63.63v4.141h1.756a.63.63 0 0 1 0 1.259M24 10.314C24 4.943 18.615.572 12 .572S0 4.943 0 10.314c0 4.811 4.27 8.842 10.035 9.608c.391.082.923.258 1.058.59c.12.301.079.766.038 1.08l-.164 1.02c-.045.301-.24 1.186 1.049.645c1.291-.539 6.916-4.078 9.436-6.975C23.176 14.393 24 12.458 24 10.314"
@@ -294,7 +296,6 @@ export function SimpleIconsLine(props: SVGProps<SVGSVGElement>) {
 
 export function MaterialSymbolsLanguage(props: SVGProps<SVGSVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
@@ -302,6 +303,7 @@ export function MaterialSymbolsLanguage(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 32 32"
       {...props}
     >
+      <title>ウェブサイト</title>
       <path
         fill="currentColor"
         d="M29 14H4l3-6h25l-3 6zm-3 10h-6l-4 8H8l4-8H0l3-6h26l-3 6zm4-18h-4l2-6h4l-2 6zm-6 0h-4l2-6h4l-2 6z"
